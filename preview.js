128990386b77f24dd7b546534308ee069c480dbb
mymoomin <20903941+mymoomin@users.noreply.github.com>
2021-11-09 17:35:35 +0000

    Initial commit

---
+++ preview.js
@@ -0,0 +1,156 @@
+const preview = document.querySelector("div.preview");
+const avatarImg = document.querySelector("img.avatar");
+const usernameSpan = document.querySelector("span.username");
+const roleSpan = document.querySelector("span.roleMention");
+const embedWrapper = document.querySelector("div.embedWrapper");
+const embedLink = document.querySelector("a.embedTitleLink");
+const embedDescription = document.querySelector("div.embedDescription");
+
+const toggleButton = document.getElementById("toggleTheme");
+
+toggleButton.addEventListener("click", toggleTheme2);
+let isDark = true;
+
+function toggleTheme() {
+  if (isDark) {
+    preview.classList.replace("theme-dark", "theme-light");
+  } else {
+    preview.classList.replace("theme-light", "theme-dark");
+  }
+  isDark = !isDark;
+  console.log("hello");
+}
+
+function toggleTheme2() {
+  preview.classList.replace("theme-dark", "theme-light") ||
+    preview.classList.replace("theme-light", "theme-dark");
+}
+
+const defaultValues = {
+  avatarURL: "/webhook_icon.png",
+  username: "RSS to Webhook",
+  title: "Default Comic",
+  color: "",
+  lastEntryTitle: "Default Comic: Page 2",
+  lastEntryUrl: "#",
+};
+
+const Preview = {
+  _avatarUrl: undefined,
+  _username: undefined,
+  _title: undefined,
+  _color: undefined,
+  _lastEntryTitle: undefined,
+  _lastEntryUrl: undefined,
+
+  feedUrl: undefined,
+
+  get avatarUrl() {
+    return _avatarURL;
+  },
+  set avatarUrl(url) {
+    if (url !== undefined) {
+      this._avatarUrl = url;
+      avatarImg.src = url;
+    }
+  },
+
+  get username() {
+    return _username;
+  },
+  set username(username) {
+    if (username !== undefined) {
+      this._username = username;
+      usernameSpan.innerHTML = username;
+    }
+  },
+
+  get title() {
+    return _title;
+  },
+  set title(title) {
+    if (title !== undefined) {
+      this._title = title;
+      roleSpan.innerHTML = `@${title}`;
+      embedDescription.innerHTML = `New ${title}!`;
+    }
+  },
+
+  get color() {
+    return _color;
+  },
+  set color(hexcode) {
+    if (hexcode !== undefined) {
+      if (hexcode) {
+        hexcode = hexcode.replace("#", "");
+        this._color = hexcode;
+        embedWrapper.style.borderLeftColor = "#" + hexcode;
+        console.log(hexcode);
+      } else {
+        this._color = hexcode;
+        embedWrapper.style.borderLeftColor = hexcode;
+        console.log(hexcode);
+      }
+    }
+  },
+
+  get lastEntryTitle() {
+    return _lastEntryTitle;
+  },
+  set lastEntryTitle(title) {
+    if (title !== undefined) {
+      this._lastEntryTitle = title;
+      embedLink.innerHTML = title;
+    }
+  },
+
+  get lastEntryUrl() {
+    return _lastEntryUrl;
+  },
+  set lastEntryUrl(url) {
+    if (url !== undefined) {
+      this._lastEntryUrl = url;
+      embedLink.href = url;
+    }
+  },
+
+  submit() {
+    return {
+      feedUrl: this.feedUrl,
+      avatarUrl: this._avatarURL,
+      username: this._username,
+      title: this._title,
+      color: this._color,
+    };
+  },
+
+  update({feedUrl, avatarUrl, username, title, color, lastEntryTitle, lastEntryUrl}) {
+      this.feedUrl = feedUrl
+      this.avatarUrl = avatarUrl
+      this.username = username
+      this.title = title
+      this.color = color
+      this.lastEntryTitle = lastEntryTitle
+      this.lastEntryUrl = lastEntryUrl
+  }
+};
+
+export default Preview;
+
+const updatePreview = ({
+  avatarURL,
+  username,
+  title,
+  color,
+  lastEntryTitle,
+  lastEntryUrl,
+}) => {
+  preview.avatarUrl = avatarURL;
+  preview.username = username;
+  preview.title = title;
+  preview.color = color;
+  preview.lastEntryTitle = lastEntryTitle;
+  preview.lastEntryUrl = lastEntryUrl;
+};
+
+// updatePreview(defaultValues)
---
+++ script.js
@@ -0,0 +1,104 @@
+import Preview from "/preview.js";
+
+const form = document.querySelector("form");
+const feedInput = document.getElementById("feed");
+const checkFeedButton = document.getElementById("checkFeed");
+const titleInput = document.getElementById("title");
+const submitButton = document.getElementById("submit");
+const colorInput = document.getElementById("color");
+
+checkFeedButton.addEventListener("click", checkFeed);
+
+async function checkFeed() {
+  if (!feedInput.checkValidity()) {
+    feedInput.reportValidity()
+    //invalid url stuff here
+    return;
+  }
+  console.log("loading");
+  const response = await getDummyData(feedInput.value);
+  switch (response.validity) {
+    case true:
+      if (!titleInput.value) titleInput.value = response.title;
+      Preview.update({...response, title: titleInput.value})
+
+      //TODO: true stuff
+      console.log("valid feed");
+      break;
+    case false:
+      //TODO: false stuff
+      console.log("invalid rss feed");
+    case "exists":
+      //TODO: false stuff
+      console.log("already exists");
+  }
+}
+
+async function getDummyData() {
+  alert(
+    "Haven't added the backend yet, so this just gets the data for Sleepless Domain for now"
+  );
+  await fetch("/");
+  return {
+    validity: true,
+    title: "Sleepless Domain",
+    lastEntryTitle: "Sleepless Domain - Chapter ??? - Page 8",
+    lastEntryUrl: "https://www.sleeplessdomain.com/comic/chapter-page-8",
+  };
+}
+
+feedInput.addEventListener("input", validateUrl);
+
+function validateUrl() {
+  if (!/https?:\/\/.+\..+/.test(feedInput.value)) {
+    feedInput.setCustomValidity("Please enter a valid URL");
+  } else {
+    feedInput.setCustomValidity("");
+  }
+}
+
+colorInput.addEventListener("input", validateColor);
+
+function validateColor() {
+  let message = "";
+  if (!/^$|#?[\da-fA-F]{6}/.test(colorInput.value)) {
+    if (!/#?.{6}/.test(colorInput.value)) {
+      message += "Must be 6 digits. ";
+    }
+    if (!/^#?[\da-fA-F]*$/.test(colorInput.value)) {
+      message += "\nDigits must be either numbers 0-9 or letters A-F";
+    }
+  }
+  colorInput.setCustomValidity(message);
+}
+
+form.addEventListener("submit", handleFormSubmit);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  console.log("submit");
+  if (form.checkValidity() === false) {
+    console.log("not valid");
+    event.preventDefault();
+  } else {
+    // On a production site do form submission.
+    submitButton.disabled = "true";
+    alert("If the backend worked, this would add the comic!");
+    event.preventDefault();
+    submitButton.disabled = undefined;
+  }
+}
+
+for (const elem of form.elements) {
+  if (elem.nodeName === "INPUT") {
+    elem.addEventListener("input", updatePreview);
+  }
+}
+
+function updatePreview(event) {
+  if (event.target.checkValidity()) {
+    Preview[event.target.name] = event.target.value;
+  }
+}
+
+console.log("hi");
